b7628749ce534394d3d58aea1e816d7861cbcbb9
allobanov <wrider@yandex.ru>
2025-02-15 13:45:17 +0300

    pagination comments added

--- app/app.py
+++ app/app.py
@@ -1,19 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_admin import Admin
 from flask_login import LoginManager
 
-from models import Wine, User
-from werkzeug.security import generate_password_hash
+from models import Wine, User, Comment
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_admin.contrib.sqla import ModelView
 
 
 from db import db_session
 
-from flask_login import LoginManager, login_user, current_user
-from flask_paginate import Pagination, get_page_parameter, get_page_args
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
-
-
+from flask_paginate import Pagination, get_page_args
 
 
 
@@ -23,13 +21,23 @@ app.config["SECRET_KEY"] = "you-will-never-guess"
 admin = Admin(app, name='wineproject')
 admin.add_view(ModelView(User, db_session))
 admin.add_view(ModelView(Wine, db_session))
+admin.add_view(ModelView(Comment, db_session))
 
 login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 
 @app.route("/")
 def index():
+    if "username" in session:
+        return redirect(url_for("dashboard"))
     return render_template("index.html")
+
 
 
 @app.route("/catalog")
@@ -37,15 +45,12 @@ def catalog():
 
     wines = Wine.query.all()
 
-
     def get_wines(offset=0, per_page=30):
         return wines[offset: offset + per_page]
-
 
     search = False
     total = len(wines)
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-
 
     pagination_wines = get_wines(offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total, search=search, record_name='позиции')
@@ -53,15 +58,20 @@ def catalog():
     return render_template("catalog.html", data=pagination_wines, pagination=pagination, page=page, per_page=per_page)
 
 
+
 @app.route("/wine/<int:id>")
 def wine_card(id):
     wine = Wine.query.get(id)
-    return render_template("wine.html", data=wine)
+    # comm = Comment.query.get(id)
+    comm = Comment.query.filter_by(wine_id=id).all()
+    return render_template("wine.html", data=wine, comm=comm)
+
 
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -72,36 +82,90 @@ def register():
             email = request.form.get('email')
             username = request.form.get('username')
             new_user = User(email=email, username=username, password_hash=hash)
-
-            # try:
             db_session.add(new_user)
             db_session.commit()
             flash("вы успешно зарегистрировались")
-            # except Exception as exc:
-            #     print(exc)
          
-
         else:
             flash('Неверно введены данные')
-
 
     return render_template('register.html')
 
 
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username and password:
+            user = User.query.filter_by(username=username).first()
+            
+            if user and check_password_hash(user.password_hash, password):
+                flash(f"Вы вошли как {username}")
+                login_user(user)
+
+                next_page = request.args.get('next')
+
+                redirect(next_page)
+            else:
+                flash("Логин или пароль не корректен")
+        else:
+            flash("Заполните поля почта и пароль")
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
 
     return render_template("login.html")
+
+
 
 
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
 
+
 @app.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/lgn')
+@login_required
+def admin_index():
+    if current_user.is_admin:
+        return 'Привет админ'
+    else:
+        return 'Ты не админ!'
+
+
+
+@app.route("/create-comment/<wine_id>", methods=["POST"])
+@login_required
+def create_comment(wine_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Комментарий не может быть пустым', category='error')
+    else:
+        wine = Wine.query.filter_by(id = wine_id)
+        if wine:
+            comment = Comment(text=text, author=current_user.id, wine_id=wine_id)
+            db_session.add(comment)
+            db_session.commit()
+            print(comment)
+            flash("Комментарий сохранен")
+        else:
+            flash("Комментариев не найдено", category="error")
+
+        
+    return redirect(request.referrer)
+
 
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
 
 from db import Base, engine
 
@@ -10,17 +13,21 @@ class User(UserMixin, Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(50), index=True, unique=True)
     password_hash = Column(String(256), nullable=False)
-    role = Column(String(10), index=True)
+    role = Column(String(10), index=True, default='user')
     email = Column(String(50), unique=True, nullable=False)
+    # comm = relationship("Comment")
 
     def __repr__(self):
-        return f"User {self.id}, {self.username}"
+        return self.username
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
     
 class Wine(Base):
     __tablename__ = "wines"
@@ -35,9 +42,24 @@ class Wine(Base):
     avg_price = Column(String(10))
     viv_rating = Column(String(10))
     img_link = Column(String(100))
+    comment = relationship("Comment")
 
     def __repr__(self):
         return self.wine_name
+    
+class Comment(Base):
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key=True)
+    author = Column(Integer, ForeignKey(User.id))
+    wine_id = Column(Integer, ForeignKey(Wine.id))
+    text = Column(String(350), nullable=False)
+    date_created = Column(DateTime, default=datetime.now)
+    
+    def __repr__(self):
+        return f'Comment({self.text}, {self.date_created.strftime("%d.%m.%Y-%H.%M")}, {self.wine_id})'
+
+    user = relationship("User")
+    wine = relationship("Wine")
 
     
 if __name__ == "__main__":
